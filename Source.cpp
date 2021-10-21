3b0be1b58828cb8952203994ea73392222f878eb
AlexeyShuts <92624548+AlexeyShuts@users.noreply.github.com>
2021-10-21 20:56:29 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,41 @@
+﻿//Alex Shuts
+#include <iostream>
+using namespace std;
+int main() {
+	/*
+	Boolean16◦
+.Дано целое положительное число.
+Проверить истинность высказывания :
+«Данное число является четным двузначным».
+*/
+int a;
+bool res;
+cin >> a;
+res = a > 0;
+cout << boolalpha << res << endl;
+	return 0;
+}
+/*
+Boolean17◦
+Дано целое положительное число.
+Проверить истинность высказывания:
+«Данное число является нечетным трехзначным».
+*/
+int a;
+bool res;
+cin >> a;
+res = a > 0;
+cout; << boolalpha << res << endl
+/*
+Boolean21◦
+. Дано трехзначное число.
+Проверить истинность высказывания:
+«Цифры данного числа образуют возрастающую последовательность».
+*/
+int a;
+bool res;
+cin >> a1, a2,a3;
+res = a1 < a2 < a3;
+res = a2 > a1 < a3;
+res = a3 > a2 > a1;
+cuot; << boolalpha<<res<<endl
